875d4f76aea2c5d958f80ebc20b899e86e40974c
SaiShivan <shivan@gmail.com>
2020-11-05 11:13:59 +0530

    Char

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        char myUnicodeChar = '\u0044';
+        System.out.println( myUnicodeChar);
+
+        String myString = "hi";
+        System.out.println("my string is" + myString);
+
+    }
+}
